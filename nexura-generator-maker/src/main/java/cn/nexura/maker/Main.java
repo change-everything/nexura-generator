a24abd06693d3f142fb876866b9c97e4552a19ea
peiyp <1158336066@qq.com>
2023-12-26 22:44:38 +0800

    模版设计器完成

--- nexura-generator-maker/src/main/java/cn/nexura/maker/Main.java
+++ nexura-generator-maker/src/main/java/cn/nexura/maker/Main.java
@@ -1,8 +1,6 @@
 package cn.nexura.maker;
 
 import cn.nexura.maker.generaror.main.MainGenerator;
-import cn.nexura.maker.meta.Meta;
-import cn.nexura.maker.meta.MetaManager;
 import freemarker.template.TemplateException;
 
 import java.io.IOException;
--- nexura-generator-maker/src/main/java/cn/nexura/maker/generaror/JarGenerator.java
+++ nexura-generator-maker/src/main/java/cn/nexura/maker/generaror/JarGenerator.java
@@ -11,7 +11,7 @@ public class JarGenerator {
     public static void doGenerate(String projectDir) throws IOException, InterruptedException {
         String winMavenCommand = "mvn.cmd clean package -DskipTests=true";
         String otherMavenCommand = "mvn clean package -DskipTests=true";
-        String mavenCommand = winMavenCommand;
+        String mavenCommand = otherMavenCommand;
 
         ProcessBuilder processBuilder = new ProcessBuilder(mavenCommand.split(" "));
 
